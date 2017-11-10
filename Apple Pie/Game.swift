5093b29542a631ce41082d4eac448696e9c77b58
Gavin Schipper <gavin_schipper@hotmail.com>
2017-11-10 11:21:37 +0100

    Final commit

---
+++ Apple Pie/Game.swift
@@ -0,0 +1,34 @@
+//
+//  Game.swift
+//  Apple Pie
+//
+//  Created by Gavin Schipper on 06-11-17.
+//  Copyright © 2017 Gavin Schipper. All rights reserved.
+//
+
+import Foundation
+
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+    var guessedLetters: [Character]
+    
+    mutating func playerGuessed(letter: Character) {
+        guessedLetters.append(letter)
+        if !word.characters.contains(letter) {
+            incorrectMovesRemaining -= 1
+        }
+    }
+    
+    var formattedWord: String {
+        var guessedWord = ""
+        for letter in word.characters {
+            if guessedLetters.contains(letter) {
+                guessedWord += "\(letter)"
+            } else {
+                guessedWord += "_"
+            }
+        }
+    return guessedWord
+    }
+}
